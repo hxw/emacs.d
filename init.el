2f5155540928ac1c1e8dc54000ab52ad95ae1ad8
Christopher Hall <hsw@client-201.lan1.generalresources.com>
2007-12-06 13:32:25 +0800

    Xrefactory configuration

--- init.el
+++ init.el
@@ -264,3 +264,12 @@
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
+;; Xrefactory configuration part ;;
+;; some Xrefactory defaults can be set here
+(defvar xref-current-project nil) ;; can be also "my_project_name"
+(defvar xref-key-binding 'global) ;; can be also 'local or 'none
+(setq load-path (cons "/data/home/hsw/Xrefactory/xref/emacs" load-path))
+(setq exec-path (cons "/data/home/hsw/Xrefactory/xref" exec-path))
+(load "xrefactory")
+;; end of Xrefactory configuration part ;;
+(message "xrefactory loaded")
