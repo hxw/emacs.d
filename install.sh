e5313cd0ec65c9bbe478b19a7735308b7355a4e6
Christopher Hall <hsw@openmoko.com>
2010-11-17 22:12:58 +0800

    minimum packages to start EMACS (on kubuntu)
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# packages needed to run EMACS
+
+apt-get install emacs23 emacs23-el
+apt-get install emacs-goodies-el
+apt-get install ocaml-mode haskell-mode
+apt-get install slime
+apt-get install pyflakes
