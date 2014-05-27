dafa9180c4bb8164053f85df4bd1a5d2af64c3cd
Christopher Hall <hsw@ms2.hinet.net>
2014-05-27 11:13:12 +0800

    js2 indent changes to 4
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
  '(js-indent-level 2)
  '(js2-allow-keywords-as-property-names nil)
  '(js2-auto-indent-p t)
- '(js2-basic-offset 2)
+ '(js2-basic-offset 4)
  '(js2-bounce-indent-p nil)
  '(js2-cleanup-whitespace t)
  '(js2-enter-indents-newline t)
