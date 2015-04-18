9842a3736c66694979521de5d9ff7d2e76e187e0
Christopher Hall <hsw@ms2.hinet.net>
2015-04-18 10:48:36 +0800

    add melpa.org pacakages
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- init.el
+++ init.el
@@ -11,6 +11,21 @@
                  (list (concat init-dir "/lisp"))
                  load-path))
 
+;; melpa.org packages
+;; ------------------
+(require 'package)
+
+(if t
+    (add-to-list 'package-archives
+                 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/") t))
+
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
 
 ;; enable or disable some options
 ;; ------------------------------
