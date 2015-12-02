4aee2c8322e0c0f08580c2e958e070ba85e2f2a8
Christopher Hall <hsw@ms2.hinet.net>
2015-12-02 14:36:10 +0800

    remove obsolete script
    Signed-off-by: Christopher Hall <hsw@ms2.hinet.net>

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# packages needed to run EMACS
-
-apt-get install emacs23 emacs23-el
-apt-get install emacs-goodies-el
-apt-get install ocaml-mode haskell-mode
-apt-get install slime
-apt-get install pyflakes
-apt-get install yasnippet
